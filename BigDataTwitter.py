d415f695d17929cf860923402d1723deac869412
Denetax <mehdistocky@gmail.com>
2016-05-19 16:54:23 +0200

    Ajout template + info

--- BigDataTwitter.py
+++ BigDataTwitter.py
@@ -1,5 +1,7 @@
 from flask import *
 from pymongo import MongoClient
+from datetime import datetime
+
 import requests
 
 app = Flask(__name__)
@@ -8,21 +10,36 @@ def query():
     client = MongoClient('localhost', 27017)
     #db = client['BigDataTwitter']
     db = client.BigDataTwitter
-    collection = db.data
+    collection = db.dataTwitter
     return collection
 
 @app.route('/')
 def index():
-    # = query().find_one()
+
     Tweet = []
+    tabData = []
     Count = {}
+    retweet = {}
+    test = 0;
+    maintenant = datetime.now()
+    print(maintenant)
+    print(maintenant)
+
     Count["nbrAbonner"] = query().find().count()
-    yo = query().find({},{"name":1,"date":1, "_id":0})
+    DataDate = query().find({},{"date":1, "_id":0})
+    for element in DataDate :
+        tabData.append(str(element["date"]).split(" ")[0])
+        tabData = list(set(tabData))
+    ElementMoyenne = [query().find({"date" : {"$regex": element}}).count() for element in tabData]
+    for element in ElementMoyenne :
+        test = test+element
+    print(test / len(ElementMoyenne))
+    print(str(maintenant).split(" ")[0])
+    yo = query().find({},{"retweet":1,"tweet":1,"date":1, "_id":0})
     for element in yo:
-        print(element)
-        if "date" in element :
-            Tweet.append(element)
+        Tweet.append(element)
         Count["nbrTweet"] = len(Tweet)
+    #print(maintenant)
     return render_template("Home.html",Tweet=Tweet,Count=Count)
 
 if __name__== '__main__':
---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Mehdi on 19/05/2016.
+ */
+
+$(document).ready(function() {
+    $('#InfoGfi').DataTable();
+} );
