74a16011172bb7519a13b3b0ea068e82f04d04eb
Denetax <mehdistocky@gmail.com>
2016-05-19 22:26:24 +0200

    Maj de données + interface

--- BigDataTwitter.py
+++ BigDataTwitter.py
@@ -16,31 +16,52 @@ def query():
 @app.route('/')
 def index():
 
-    Tweet = []
-    tabData = []
-    Count = {}
-    retweet = {}
-    test = 0;
-    maintenant = datetime.now()
-    print(maintenant)
-    print(maintenant)
+    Tweet = [] # Table des tweets
+    tabData = {}
+    tabDataPays = {}
+    Count = {} # liste des différents count (nbr tweet + nbr abonnée + ....)
+#    test = 0;
 
-    Count["nbrAbonner"] = query().find().count()
-    DataDate = query().find({},{"date":1, "_id":0})
+#    Count["nbrAbonner"] = query().find().count()
+
+#    DataDate = query().find({},{"date":1, "_id":0})
+#    for element in DataDate :
+#        tabData.append(str(element["date"]).split(" ")[0])
+#        tabData = list(set(tabData))
+#  ElementMoyenne = [query().find({"date" : {"$regex": element}}).count() for element in tabData]
+# for element in ElementMoyenne :
+# test = test+element
+#    print(test / len(ElementMoyenne))
+#    print(str(maintenant).split(" ")[0])
+
+    #DataDate = list(set(DataDate))
+    followersCount = query().find({},{"followers_count" : 1})
+    Count["nbrPeopleToucher"] = 0
+    for element in followersCount :
+        Count["nbrPeopleToucher"]= int(Count["nbrPeopleToucher"]) + int(element["followers_count"])
+
+    DataDate = query().find({}, {"tweetos": 1, "_id": 0})
     for element in DataDate :
-        tabData.append(str(element["date"]).split(" ")[0])
-        tabData = list(set(tabData))
-    ElementMoyenne = [query().find({"date" : {"$regex": element}}).count() for element in tabData]
-    for element in ElementMoyenne :
-        test = test+element
-    print(test / len(ElementMoyenne))
-    print(str(maintenant).split(" ")[0])
-    yo = query().find({},{"retweet":1,"tweet":1,"date":1, "_id":0})
+        if(element["tweetos"] in  tabData):
+            tabData[element["tweetos"]] = tabData[element["tweetos"]]+1
+        else :
+            tabData[element["tweetos"]] = 1
+
+    DataPays = query().find({}, {"location": 1, "_id": 0})
+    for element in DataPays:
+        if (element["location"] in tabDataPays):
+            tabDataPays[element["location"]] = tabDataPays[element["location"]] + 1
+        else:
+            tabDataPays[element["location"]] = 1
+    print(tabDataPays)
+    yo = query().find({},{"retweet":1,"tweet":1,"tweetos":1,"date":1, "_id":0})
     for element in yo:
         Tweet.append(element)
-        Count["nbrTweet"] = len(Tweet)
-    #print(maintenant)
-    return render_template("Home.html",Tweet=Tweet,Count=Count)
+
+    Count["nbrPaysTweet"] = len(tabDataPays)
+    Count["nbrPeopleTweet"] = len(tabData)
+    Count["nbrTweet"] = len(Tweet)
+    return render_template("Home.html",Tweet=Tweet,Count=Count,tabData=tabData,tabDataPays=tabDataPays)
 
 if __name__== '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -4,4 +4,36 @@
 
 $(document).ready(function() {
     $('#InfoGfi').DataTable();
+    $('#InfoGfiFollowers').DataTable();
+    $('#InfoGfiPays').DataTable();
+
+    $( "#TopTweet" ).click(function() {
+        $("#TopTweet").attr("class","active");
+        $("#TopFollowers").attr("class","");
+        $("#TopPays").attr("class","");
+
+        $("#blockTopPays").attr("style","display:none;");
+        $("#blockTopFollowers").attr("style","display:none;");
+        $("#blockTopTweet").attr("style","display:inline;");
+    });
+
+    $( "#TopFollowers" ).click(function() {
+        $("#TopFollowers").attr("class","active");
+        $("#TopTweet").attr("class","");
+        $("#TopPays").attr("class","");
+
+        $("#blockTopPays").attr("style","display:none;");
+        $("#blockTopTweet").attr("style","display:none;");
+        $("#blockTopFollowers").attr("style","display:inline;");
+    });
+
+     $( "#TopPays" ).click(function() {
+        $("#TopPays").attr("class","active");
+        $("#TopTweet").attr("class","");
+        $("#TopFollowers").attr("class","");
+
+        $("#blockTopTweet").attr("style","display:none;");
+        $("#blockTopFollowers").attr("style","display:none;");
+        $("#blockTopPays").attr("style","display:inline;");
+    });
 } );
